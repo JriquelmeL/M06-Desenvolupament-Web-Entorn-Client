e4ecacc09673a30eb33a94fe8f2748fb1f95d7fc
JriquelmeL <113889948+JriquelmeL@users.noreply.github.com>
2023-01-19 11:28:36 +0100

    Add files via upload

---
+++ actividad.js
@@ -0,0 +1,7 @@
+var myPhone = {
+    marca: "apple",
+    modelo: "Iphone XS",
+    anoLanzamiento: 2018,
+};
+console.log(myPhone.marca, myPhone.modelo, myPhone.anoLanzamiento);
+document.write(myPhone.marca, myPhone.modelo, myPhone.anoLanzamiento);
